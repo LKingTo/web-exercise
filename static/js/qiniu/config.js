31ca2681e387a21169b16072b5c61af2571e2620
leungkingto <jingtao.820@163.com>
2019-05-17 17:17:34 +0800

    配置引入七牛云sdk

---
+++ static/js/qiniu/config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+	accessKey: "s1VEpG4cgWr-8N-onFRXnJVyxm_8V5HFPzP84Te9",
+	secretKey: "ATk0-WPt9Ew64jFI6w_cVHTN5yr8NBQTWwRb38KY",
+	bucket: "ktyok_img",
+	domain: "http://prla72uxr.bkt.clouddn.com/",
+	uploadToken: "s1VEpG4cgWr-8N-onFRXnJVyxm_8V5HFPzP84Te9:kuHrLr8QIo0dw_ihqXkFbiwSElI=:eyJzY29wZSI6Imt0eW9rX2ltZyIsImRlYWRsaW5lIjoxNTU4MDc3ODgxfQ=="
+}
